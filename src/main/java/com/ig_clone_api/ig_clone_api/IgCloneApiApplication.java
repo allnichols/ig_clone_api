34f1fd910611d28031de7e3197dbef136a8b1990
Max <allenmaxnichols@gmail.com>
2024-05-30 16:05:26 -0500

    initial commit

---
+++ src/main/java/com/ig_clone_api/ig_clone_api/IgCloneApiApplication.java
@@ -0,0 +1,13 @@
+package com.ig_clone_api.ig_clone_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgCloneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgCloneApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ig_clone_api/ig_clone_api/IgCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ig_clone_api.ig_clone_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgCloneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
