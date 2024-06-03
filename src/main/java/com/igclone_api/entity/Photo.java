3b30438114b349f472f0c8c8de839d557f879bd0
Max <allenmaxnichols@gmail.com>
2024-06-03 16:34:44 -0500

    add photo entity

---
+++ src/main/java/com/igclone_api/entity/Photo.java
@@ -0,0 +1,40 @@
+package com.igclone_api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "photos")
+public class Photo {
+
+    @Id
+    @SequenceGenerator(
+            name = "photo_sequence",
+            sequenceName = "id",
+            allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "photo_sequence")
+    private Long id;
+
+    @Column(nullable = false)
+    private String image_url;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private Date created_at;
+
+//    a relationship between photo and user
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
