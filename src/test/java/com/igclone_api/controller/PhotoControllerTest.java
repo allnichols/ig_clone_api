985ba34fa6c064189f0fedd11399b9ceccb094e8
Max <allenmaxnichols@gmail.com>
2024-06-04 11:01:22 -0500

    add tests to photo controller

--- src/test/java/com/igclone_api/controller/PhotoControllerTest.java
+++ src/test/java/com/igclone_api/controller/PhotoControllerTest.java
@@ -1,7 +1,30 @@
 package com.igclone_api.controller;
 
+import com.igclone_api.entity.Photo;
+import com.igclone_api.service.PhotoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class PhotoControllerTest {
 
+        @Autowired
+        private PhotoService photoService;
+
+        @org.junit.jupiter.api.Test
+        void getPhotos() {
+           assertEquals(100, photoService.getAllPhotos().size());
+        }
+
+        @org.junit.jupiter.api.Test
+        void getPhotoNull() {
+            assertNull(photoService.getPhotoById(null));
+        }
+
+        @org.junit.jupiter.api.Test
+        void getPhoto() {
+            assertNotNull(photoService.getPhotoById(1L));
+        }
 }
